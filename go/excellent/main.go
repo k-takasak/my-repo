7315a1c6d73ba84bb82199b1968ab6982e0d36c1
Keisuke Takasaki <takasaki.keisuke@isid.co.jp>
2024-09-24 13:49:59 +0900

    fix

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,13 +1,5 @@
 package main
 
-import "fmt"
-
-var version string
-
-func main() {
-	fmt.Printf("Example %s\n", version)
-}
-
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
