ae4ee2819fbb7fd6399f5f256b3f259ff614c726
Keisuke Takasaki <takasaki.keisuke@isid.co.jp>
2024-09-24 13:44:51 +0900

    add release

--- go/excellent/main.go
+++ go/excellent/main.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
+var version string
+
+func main() {
+	fmt.Printf("Example %s\n", version)
+}
+
 func EvenOrOdd(number int) string {
 	if number%2 == 0 {
 		return "even"
